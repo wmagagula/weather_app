a91a1396e629f8f8b53e47e6e936073fa0adb996
warrenmagagula <magagulaw@out.co.za>
2024-09-05 03:02:12 +0200

    Move multiprovider up the widget tree. add easy loader init, inject httpWeatherRepo on WeatherProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:open_weather_example_flutter/src/api/api_keys.dart';
 import 'package:open_weather_example_flutter/src/features/weather/application/providers.dart';
-import 'package:open_weather_example_flutter/src/features/weather/presentation/weather_page.dart';
+import 'package:open_weather_example_flutter/src/features/weather/data/weather_repository.dart';
+import 'package:open_weather_example_flutter/src/features/weather/presentation/views/weather_page.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   setupInjection();
+  injectWeatherRepository();
   runApp(const MyApp());
 }
 
@@ -22,30 +25,31 @@ class MyApp extends StatelessWidget {
         offset: const Offset(0, 0.5),
       )
     ]);
-    return MaterialApp(
-      title: 'Flutter Weather App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        textTheme: TextTheme(
-          displayLarge: textStyleWithShadow,
-          displayMedium: textStyleWithShadow,
-          displaySmall: textStyleWithShadow,
-          headlineMedium: textStyleWithShadow,
-          headlineSmall: textStyleWithShadow,
-          titleMedium: const TextStyle(color: Colors.white),
-          bodyMedium: const TextStyle(color: Colors.white),
-          bodyLarge: const TextStyle(color: Colors.white),
-          bodySmall: const TextStyle(color: Colors.white70, fontSize: 13),
-        ),
-      ),
-      home: MultiProvider(
-          providers: [
-            ChangeNotifierProvider<WeatherProvider>(create: (_) => WeatherProvider(), lazy: false),
-          ],
-          builder: (context, _) {
-            return const WeatherPage(city: 'London');
-          }),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<WeatherProvider>(
+            create: (_) => WeatherProvider(repository: sl<HttpWeatherRepository>(instanceName: 'weather_repo')),
+            lazy: false),
+      ],
+      child: MaterialApp(
+          title: 'Open Weather',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            textTheme: TextTheme(
+              displayLarge: textStyleWithShadow,
+              displayMedium: textStyleWithShadow,
+              displaySmall: textStyleWithShadow,
+              headlineMedium: textStyleWithShadow,
+              headlineSmall: textStyleWithShadow,
+              titleMedium: const TextStyle(color: Colors.white),
+              bodyMedium: const TextStyle(color: Colors.white),
+              bodyLarge: const TextStyle(color: Colors.white),
+              bodySmall: const TextStyle(color: Colors.white70, fontSize: 13),
+            ),
+          ),
+          builder: EasyLoading.init(),
+          home: const WeatherPage(city: 'London')),
     );
   }
 }
